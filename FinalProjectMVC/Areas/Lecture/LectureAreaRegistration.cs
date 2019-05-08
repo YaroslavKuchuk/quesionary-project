b7d714c7742876468109dc1ae468251f06beda62
tojarr <30999615+tojarr@users.noreply.github.com>
2019-05-08 17:19:34 +0300

    Add areas leyers to MVC project

---
+++ FinalProjectMVC/Areas/Lecture/LectureAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace FinalProjectMVC.Areas.Lecture
+{
+    public class LectureAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "Lecture";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "Lecture_default",
+                "Lecture/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
