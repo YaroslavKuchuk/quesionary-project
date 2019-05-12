afd035294e726a560bcf260ef4b554106534e7ce
tojarr <30999615+tojarr@users.noreply.github.com>
2019-05-12 18:57:32 +0300

    Added GroupController to Admin area

---
+++ FinalProjectMVC/Areas/Admin/Models/Groups/CreateGroupVm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinalProjectMVC.Areas.Admin.Models.Groups
+{
+    public class CreateGroupVm
+    {
+        public string Name { get; set; }
+    }
+}
