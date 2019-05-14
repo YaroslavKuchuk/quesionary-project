b42a7e58de791e9a70bb6596b607e7a6f576b02f
tojarr <30999615+tojarr@users.noreply.github.com>
2019-05-14 22:12:44 +0300

    Update models from Database

--- FinalProject.EFLayer/DataModels/sysdiagram.cs
+++ FinalProject.EFLayer/DataModels/sysdiagram.cs
@@ -12,15 +12,12 @@ namespace FinalProject.EFLayer.DataModels
     using System;
     using System.Collections.Generic;
     
-    public partial class TextAnswer
+    public partial class sysdiagram
     {
-        public int Id { get; set; }
-        public Nullable<int> QuestionId { get; set; }
-        public string Textbox { get; set; }
-        public Nullable<int> IsCorrect { get; set; }
-        public Nullable<bool> IsDeleted { get; set; }
-        public string TextBox { get; set; }
-    
-        public virtual Question Question { get; set; }
+        public string name { get; set; }
+        public int principal_id { get; set; }
+        public int diagram_id { get; set; }
+        public Nullable<int> version { get; set; }
+        public byte[] definition { get; set; }
     }
 }
